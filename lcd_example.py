3c6ebdc25880424f5a597a943eea33150096ec03
CaptainStouf <stouf@hardware-libre.org>
2014-03-14 01:00:16 +0100

    Example script
    added lcd_example.py

---
+++ lcd_example.py
@@ -0,0 +1,15 @@
+# on importe le pilote
+import lcddriver
+from time import *
+
+# on initialise le lcd
+lcd = lcddriver.lcd()
+
+# on reinitialise le lcd
+lcd.lcd_clear()
+
+# on affiche des caracteres sur chaque ligne
+lcd.lcd_display_string("   Hello world !", 1)
+lcd.lcd_display_string("      Je suis", 2)
+lcd.lcd_display_string("        un", 3)
+lcd.lcd_display_string("   Raspberry Pi !", 4)
